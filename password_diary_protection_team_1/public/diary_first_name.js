45df3085be6c2f0ab65f293dff1b4bbab44e1e43
darkmatter12398 <77501024+darkmatter12398@users.noreply.github.com>
2022-12-04 02:15:31 -0500

    deleted

--- password_diary_protection_team_1/public/diary_first_name.js
+++
@@ -1,6 +0,0 @@
-//to play the project, typing in "node app.js" in the terminal, and type in "localhost:3000" in any browser
-$(document).ready(function(){
-    $.post('/diary_start/first_name', function(data){
-        document.getElementById('setupHeading').innerHTML = data + "'s" + document.getElementById('setupHeading').innerHTML
-    })
-})
